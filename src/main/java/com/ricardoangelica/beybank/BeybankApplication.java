b4c4d8a6d4c3a4c26fa73aa90e3a342baf5fed55
Ricardo Yuri <127852978+ricardoyuuri@users.noreply.github.com>
2024-03-22 08:07:03 -0300

    Add files via upload

---
+++ src/main/java/com/ricardoangelica/beybank/BeybankApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoangelica.beybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardoangelica/beybank/BeybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardoangelica.beybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
